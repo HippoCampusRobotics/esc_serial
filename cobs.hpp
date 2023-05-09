7d3dc5159b48084813cad7d18d1ae27c6cb33ce3
Thies Lennart Alff <lennart.alff@gmail.com>
2023-05-09 14:46:39 +0200

    initial commit

---
+++ cobs.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdint.h>
+
+/**
+ * @brief
+ *
+ * @param _buffer
+ * @param _length Total length including COBS byte at the start and the
+ * delimiter byte at the end.
+ */
+void cobs_encode(uint8_t *_buffer, int _length);
+/**
+ * @brief
+ *
+ * @param _buffer
+ * @param _length Total length including COBS byte at the start and the
+ * delimiter byte at the end.
+ * @return uint8_t* Start of the decoded data, i.e. without the COBS byte at the
+ * start and the delimiter bye at the end.
+ */
+uint8_t *cobs_decode(uint8_t *_buffer, int _length);
